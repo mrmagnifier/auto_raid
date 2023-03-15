9cc3fcc9db1be17e8fbaee23b9d3618b84eaa4da
mrmagnifier <123655732+mrmagnifier@users.noreply.github.com>
2023-03-15 17:35:23 +0100

    Update setup_raid.sh

--- setup_raid.sh
+++ setup_raid.sh
@@ -7,9 +7,9 @@ sudo apt-get install -y mdadm
 # Auto-detect available disks excluding /dev/sda
 disks=$(lsblk -d -n -o NAME | awk '{ print "/dev/"$1 }' | grep -v "md" | grep -v "sda")
 
-# Wipe all detected disks except /dev/sda
+# Wipe all detected disks except /dev/sda using wipefs
 for disk in $disks; do
-    sudo dd if=/dev/zero of="$disk" bs=1M count=100
+    sudo wipefs -af "$disk"
 done
 
 # Create RAID array
@@ -18,7 +18,7 @@ if [ "$(echo "$disks" | wc -l)" -lt 2 ]; then
     exit 1
 fi
 
-raid_level=0  # Set the desired RAID level
+raid_level=1  # Set the desired RAID level
 
 sudo mdadm --create --verbose /dev/md0 --level="$raid_level" --raid-devices=$(echo "$disks" | wc -l) $disks
 
@@ -37,5 +37,8 @@ sudo update-initramfs -u
 # Add the RAID array to /etc/fstab
 echo '/dev/md0 /home ext4 defaults,nofail,discard 0 0' | sudo tee -a /etc/fstab
 
-# Output of /home
+# Wipe History
+sudo history -c
+
+# Output of home
 sudo df -h /home
