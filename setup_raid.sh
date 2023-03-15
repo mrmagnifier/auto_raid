71006b182b61c856d9e2119eed17ea7f791140a0
mrmagnifier <123655732+mrmagnifier@users.noreply.github.com>
2023-03-15 20:45:36 +0100

    Update setup_raid.sh

--- setup_raid.sh
+++ setup_raid.sh
@@ -1,41 +1,64 @@
 #!/bin/bash
 
-# Update package list and install mdadm
-sudo apt-get update
-sudo apt-get install -y mdadm
+# Calculate approximate progress steps
+total_steps=8
+current_step=0
 
-# Auto-detect available disks excluding /dev/sda
-disks=$(lsblk -d -n -o NAME | awk '{ print "/dev/"$1 }' | grep -v "md" | grep -v "sda")
+# Function to update and display the progress percentage
+update_progress() {
+    current_step=$((current_step + 1))
+    progress_percentage=$((current_step * 100 / total_steps))
+    echo -ne "Progress: $progress_percentage%\\r"
+}
 
-# Wipe all detected disks except /dev/sda using wipefs
-for disk in $disks; do
-    sudo wipefs -af "$disk"
-done
+# Run the script in a subshell
+(
+    # Update package list and install mdadm
+    sudo apt-get update >/dev/null 2>&1
+    sudo apt-get install -y mdadm >/dev/null 2>&1
+    update_progress
 
-# Create RAID array
-if [ "$(echo "$disks" | wc -l)" -lt 2 ]; then
-    echo "Not enough disks detected for RAID. Exiting."
-    exit 1
-fi
+    # Auto-detect available disks excluding /dev/sda
+    disks=$(lsblk -d -n -o NAME | awk '{ print "/dev/"$1 }' | grep -v "md" | grep -v "sda")
+    update_progress
 
-raid_level=0  # Set the desired RAID level
+    # Wipe all detected disks except /dev/sda using wipefs
+    for disk in $disks; do
+        sudo wipefs -af "$disk" >/dev/null 2>&1
+    done
+    update_progress
 
-sudo mdadm --create --verbose /dev/md0 --level="$raid_level" --raid-devices=$(echo "$disks" | wc -l) $disks
+    # Check disk count
+    if [ "$(echo "$disks" | wc -l)" -lt 2 ]; then
+        echo "Not enough disks detected for RAID. Exiting."
+        exit 1
+    fi
 
-# Format the RAID array
-sudo mkfs.ext4 -F /dev/md0
+    raid_level=0  # Set the desired RAID level
 
-# Mount the RAID array
-sudo mount /dev/md0 /home
+    sudo mdadm --create --verbose /dev/md0 --level="$raid_level" --raid-devices=$(echo "$disks" | wc -l) $disks >/dev/null 2>&1
+    update_progress
 
-# Update mdadm.conf
-sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf
+    # Format the RAID array
+    sudo mkfs.ext4 -F /dev/md0 >/dev/null 2>&1
+    update_progress
 
-# Update initramfs
-sudo update-initramfs -u
+    # Mount the RAID array
+    sudo mount /dev/md0 /home >/dev/null 2>&1
+    update_progress
 
-# Add the RAID array to /etc/fstab
-echo '/dev/md0 /home ext4 defaults,nofail,discard 0 0' | sudo tee -a /etc/fstab
+    # Update mdadm.conf
+    sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf >/dev/null 2>&1
+    update_progress
 
-# Output of /home
+    # Update initramfs
+    sudo update-initramfs -u >/dev/null 2>&1
+    update_progress
+
+    # Add the RAID array to /etc/fstab
+    echo '/dev/md0 /home ext4 defaults,nofail,discard 0 0' | sudo tee -a /etc/fstab >/dev/null 2>&1
+)
+
+# Notify when the script is finished
+echo -e "\nThe script is finished."
 sudo df -h /home
