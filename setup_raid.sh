9a0eca5cd4d3cf8af6292b136734dda04e78a528
mrmagnifier <123655732+mrmagnifier@users.noreply.github.com>
2023-03-15 17:51:21 +0100

    Update setup_raid.sh

--- setup_raid.sh
+++ setup_raid.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Define a simple progress bar function
+progress_bar() {
+    bar="##################################################"
+    bar_length=${#bar}
+    i=0
+    while [ $i -le $bar_length ]; do
+        printf "|%-${bar_length}s|\r" "${bar:0:$i}"
+        sleep 1
+        i=$((i + 1))
+    done
+    echo
+}
+
 # Update package list and install mdadm
 sudo apt-get update
 sudo apt-get install -y mdadm
@@ -8,8 +21,10 @@ sudo apt-get install -y mdadm
 disks=$(lsblk -d -n -o NAME | awk '{ print "/dev/"$1 }' | grep -v "md" | grep -v "sda")
 
 # Wipe all detected disks except /dev/sda using wipefs
+echo "Wiping disks..."
 for disk in $disks; do
-    sudo wipefs -af "$disk"
+    sudo wipefs -af "$disk" &
+    progress_bar
 done
 
 # Create RAID array
@@ -19,26 +34,29 @@ if [ "$(echo "$disks" | wc -l)" -lt 2 ]; then
 fi
 
 raid_level=1  # Set the desired RAID level
-
+echo "Creating RAID array..."
 sudo mdadm --create --verbose /dev/md0 --level="$raid_level" --raid-devices=$(echo "$disks" | wc -l) $disks
 
 # Format the RAID array
+echo "Formatting RAID array..."
 sudo mkfs.ext4 -F /dev/md0
 
 # Mount the RAID array
+echo "Mounting RAID array..."
 sudo mount /dev/md0 /home
 
 # Update mdadm.conf
+echo "Updating mdadm.conf..."
 sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf
 
 # Update initramfs
+echo "Updating initramfs..."
 sudo update-initramfs -u
 
 # Add the RAID array to /etc/fstab
+echo "Updating /etc/fstab..."
 echo '/dev/md0 /home ext4 defaults,nofail,discard 0 0' | sudo tee -a /etc/fstab
 
-# Wipe History
-sudo history -c
-
-# Output of home
-sudo df -h /home
+# Display disk space usage
+echo "Script finished. Displaying disk space usage:"
+df -h
