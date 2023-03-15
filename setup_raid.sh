b3998ddc2e9e8e1f82d65eb0537d1261661e2d05
mrmagnifier <123655732+mrmagnifier@users.noreply.github.com>
2023-03-15 18:01:12 +0100

    Update setup_raid.sh

--- setup_raid.sh
+++ setup_raid.sh
@@ -2,61 +2,64 @@
 
 # Define a simple progress bar function
 progress_bar() {
-    bar="##################################################"
-    bar_length=${#bar}
-    i=0
+    local duration=$1
+    local bar="##################################################"
+    local bar_length=${#bar}
+    local i=0
     while [ $i -le $bar_length ]; do
         printf "|%-${bar_length}s|\r" "${bar:0:$i}"
-        sleep 1
+        sleep "$duration"
         i=$((i + 1))
     done
     echo
 }
 
-# Update package list and install mdadm
-sudo apt-get update
-sudo apt-get install -y mdadm
+# Run the main script in the background
+(
+    # Update package list and install mdadm
+    sudo apt-get update
+    sudo apt-get install -y mdadm
 
-# Auto-detect available disks excluding /dev/sda
-disks=$(lsblk -d -n -o NAME | awk '{ print "/dev/"$1 }' | grep -v "md" | grep -v "sda")
+    # Auto-detect available disks excluding /dev/sda
+    disks=$(lsblk -d -n -o NAME | awk '{ print "/dev/"$1 }' | grep -v "md" | grep -v "sda")
 
-# Wipe all detected disks except /dev/sda using wipefs
-echo "Wiping disks..."
-for disk in $disks; do
-    sudo wipefs -af "$disk" &
-    progress_bar
-done
+    # Wipe all detected disks except /dev/sda using wipefs
+    for disk in $disks; do
+        sudo wipefs -af "$disk"
+    done
 
-# Create RAID array
-if [ "$(echo "$disks" | wc -l)" -lt 2 ]; then
-    echo "Not enough disks detected for RAID. Exiting."
-    exit 1
-fi
+    # Create RAID array
+    if [ "$(echo "$disks" | wc -l)" -lt 2 ]; then
+        echo "Not enough disks detected for RAID. Exiting."
+        exit 1
+    fi
 
-raid_level=1  # Set the desired RAID level
-echo "Creating RAID array..."
-sudo mdadm --create --verbose /dev/md0 --level="$raid_level" --raid-devices=$(echo "$disks" | wc -l) $disks
+    raid_level=1  # Set the desired RAID level
 
-# Format the RAID array
-echo "Formatting RAID array..."
-sudo mkfs.ext4 -F /dev/md0
+    sudo mdadm --create --verbose /dev/md0 --level="$raid_level" --raid-devices=$(echo "$disks" | wc -l) $disks
 
-# Mount the RAID array
-echo "Mounting RAID array..."
-sudo mount /dev/md0 /home
+    # Format the RAID array
+    sudo mkfs.ext4 -F /dev/md0
 
-# Update mdadm.conf
-echo "Updating mdadm.conf..."
-sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf
+    # Mount the RAID array
+    sudo mount /dev/md0 /home
 
-# Update initramfs
-echo "Updating initramfs..."
-sudo update-initramfs -u
+    # Update mdadm.conf
+    sudo mdadm --detail --scan | sudo tee -a /etc/mdadm/mdadm.conf
 
-# Add the RAID array to /etc/fstab
-echo "Updating /etc/fstab..."
-echo '/dev/md0 /home ext4 defaults,nofail,discard 0 0' | sudo tee -a /etc/fstab
+    # Update initramfs
+    sudo update-initramfs -u
 
-# Display disk space usage
-echo "Script finished. Displaying disk space usage:"
-df -h
+    # Add the RAID array to /etc/fstab
+    echo '/dev/md0 /home ext4 defaults,nofail,discard 0 0' | sudo tee -a /etc/fstab
+
+    # Wipe History
+    sudo history -c
+) &
+
+# Display progress bar while the script is running in the background
+progress_bar 5
+
+# Print completion message and output of df -h /home
+echo "The Script is finished"
+df -h /home
